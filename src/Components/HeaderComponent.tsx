de98003f33f4ebd8b9d54f7037647cfd6c4ecbd7
Burak Kağan Özkan <kagan1996@gmail.com>
2024-05-08 17:11:51 +0300

    initial

---
+++ src/Components/HeaderComponent.tsx
@@ -0,0 +1,12 @@
+const HeaderComponent = () => {
+
+
+    return (
+        <div className="flex justify-evenly ">
+            <h5 className="font-bold font-roboto text-[#78350F] text-[32px]"> Collers</h5>
+            <div>Liste</div>
+        </div>
+    )
+}
+
+export default HeaderComponent
